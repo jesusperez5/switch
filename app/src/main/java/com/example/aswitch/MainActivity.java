476d43a7c1023672307b2bcce66322882a0aec8d
jesusperez5 <jesuspepr554@gmail.com>
2024-11-03 15:15:01 +0100

    creando switch

---
+++ app/src/main/java/com/example/aswitch/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.aswitch;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ImageButton image = findViewById(R.id.imageButton);
+        image.setVisibility(View.INVISIBLE);
+        Switch switchButton = findViewById(R.id.switch2);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    image.setVisibility(View.VISIBLE);
+                } else {
+                    image.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+}
